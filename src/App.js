5c22a36b9895b536d636fac5b2102e42a5cefc6e
botakveggie <minhanchoo13@gmail.com>
2022-05-11 14:34:01 +0800

    adjust topbar css

--- src/App.js
+++ src/App.js
@@ -9,16 +9,34 @@ import BottomBar from "./Components/UI/BottomBar";
 /* Main App */
 const App = () => {
   const qnas = [
-    {id: 0, qn: "What Does the Fox Say?", ans: "HELLO I AM A FOX", date: new Date(2022, 5, 1), },
-    {id: 1, qn: "What Does the Chicken Say?", ans: "POK POK POK", date: new Date(2022, 5, 2), },
-    {id: 2, qn: "What Does the Dog Say?", ans: "WOOF WOOF <3", date: new Date(2022, 5, 3), },
+    {
+      id: 0,
+      qn: "What Does the Fox Say?",
+      ans: "HELLO I AM A FOX",
+      date: new Date(2022, 5, 1),
+    },
+    {
+      id: 1,
+      qn: "What Does the Chicken Say?",
+      ans: "POK POK POK",
+      date: new Date(2022, 5, 2),
+    },
+    {
+      id: 2,
+      qn: "What Does the Dog Say?",
+      ans: "WOOF WOOF <3",
+      date: new Date(2022, 5, 3),
+    },
   ];
 
   return (
     <div>
       <div className="App">
         <div className="main">
-          <div className="App-header">Hello World!</div>
+          <div className="App-header">
+            <div className="App-header-main">Hello World!</div>
+            <div className="App-header-instructions">Use ↑ and ↓ to move!</div>
+          </div>
 
           <FlashCard qn={qnas[0].qn} ans={qnas[0].ans} date={qnas[0].date} />
           <FlashCard qn={qnas[1].qn} ans={qnas[1].ans} date={qnas[1].date} />
@@ -28,6 +46,6 @@ const App = () => {
       <BottomBar />
     </div>
   );
-}
+};
 
 export default App;
