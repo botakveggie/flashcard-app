f628577d00aa34a596c3b965ca8133740104eed7
botakveggie <minhanchoo13@gmail.com>
2022-05-12 19:36:24 +0800

    upgrade fun button

--- src/Components/FlashCard/FlashCard.js
+++ src/Components/FlashCard/FlashCard.js
@@ -2,16 +2,21 @@ import "./FlashCard.css";
 import Answer from "./Answer";
 import Card from "./Card";
 import BlueButton from "../UI/BlueButton";
+import {useState} from 'react';
 
 const FlashCard = (props) => {
+const [funbutton, setFunbutton] = useState('fun button')
+const sentences = ['fun button', 'are you having fun!!!', 'hahaha stop tickling me!']
   const clickHandler = () => {
-    console.log('Clicked!!!')
+    const newName = sentences[Math.floor(Math.random() * sentences.length)]
+    setFunbutton(newName);
+    console.log('are you having fun :D:D:D ');
   }
   return (
     <Card className="flash-card">
       <h2>Question: {props.qn}</h2>
       <Answer answer={props.ans} />
-      <BlueButton onClick={clickHandler} className='fun-button'>fun button</BlueButton>
+      <BlueButton onClick={clickHandler} className='fun-button'>{funbutton}</BlueButton>
       <div className="date-added">{props.date.toDateString()}</div>
     </Card>
   );
